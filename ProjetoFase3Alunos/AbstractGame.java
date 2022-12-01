ff77b8d34ae40d9cd59751613c0c70afec5e6148
Diogo <diogobailaoforte@gmail.com>
2022-12-01 17:45:51 +0000

    aaaa

--- ProjetoFase3Alunos/AbstractGame.java
+++ ProjetoFase3Alunos/AbstractGame.java
@@ -1,7 +1,39 @@
 public abstract class AbstractGame {
+    /*tem o construtor AbstractGame(int r, int c, int diff, Symbol empty, Symbol[] values, Random gen, Eliminator elim, Accomodator acc)
+     *  vai ter área de jogo de r(rows) por c(columns), com diff(iculty) linhas já preenchidas
+     *   as linhas do diff vão estar preenchidas com símbolos escolhidos através dos values, gerados pelo gerador "gen"
+     *   o jogo vai ter de usar o elim(inator) e o acc(omodator) em todas as jogadas;
+     */ 
+    
+    int linesInGrid(){
+
+    }
+    int colsInGrid() {
+
+    }
+	void permutatePiece(int n){
+
+    }
+    void placePiece(int col) {
+
+    }	 
+    void generatePiece() {
+
+    }
+    boolean finished(){
+    /* devolve true se	não	existe	nenhuma	coluna	 onde ainda	caiba uma peça; */
+    }
+    int spaceInColumn(int col)	{
 
 
+    } 	 
+    String currentPiece(){
 
+    }
 
+    String toString(){
+        /* devolve a representação textual deste jogo; */
+
+    } 
 
 }
--- ProjetoFase3Alunos/MultipleRewardGame.java
+++ ProjetoFase3Alunos/MultipleRewardGame.java
@@ -1,4 +1,14 @@
 public class MultipleRewardGame extends AbstractGame{
-    
+    /*
+     * instancias desta subclasse representam jogos, cuja pontuação é calculada da mesma maneira q na fase 2
+     * jogos terminam se n existir coluna em q caiba alguma peça
+     * 
+     */
+    MultipleRewardGame(int r, int c, int diff, Symbol empty, Symbol[] values, Random gen, Eliminator elim, Accomodator acc){
+    /*
+     * inicializa nojo objeto "jogo"
+     */
+
+    }
     
 }
