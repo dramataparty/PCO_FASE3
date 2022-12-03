2d5195b86347b0c75bbb9169649ac8dc224cf06e
Diogo <diogobailaoforte@gmail.com>
2022-12-03 19:13:58 +0000

    startofprogress

--- ProjetoFase3Alunos/AbstractGame.java
+++ ProjetoFase3Alunos/AbstractGame.java
@@ -1,3 +1,7 @@
+import java.util.Random;
+
+
+
 public abstract class AbstractGame implements Game {
     /*tem o construtor AbstractGame(int r, int c, int diff, Symbol empty, Symbol[] values, Random gen, Eliminator elim, Accomodator acc)
      *  vai ter área de jogo de r(rows) por c(columns), com diff(iculty) linhas já preenchidas
@@ -5,25 +9,53 @@ public abstract class AbstractGame implements Game {
      *   o jogo vai ter de usar o elim(inator) e o acc(omodator) em todas as jogadas;
      */ 
     
+
+    private int r;
+    private int c;
+    private int diff;
+    private Symbol empty;
+    private Symbol[] values;
+    private Accomadator acc;
+    private Eliminator elim;
+    private Random gen;
+
+
+
+    public AbstractGame(int r, int c, int diff, Symbol empty, Symbol[]
+    values, Random gen, Eliminator elim, Accomodator acc){
+        this.r = r;
+        this.c = c;
+        this.diff=diff;
+        this.empty=empty;
+        this.values=values;
+        this.acc=acc;
+        this.elim = elim;
+        this.gen = gen;
+       
+    }
+
+    public int linesInGrid(){
+        return r;
+        
+    }
+    public int colsInGrid() {
+        return c;
+    }
     @Override
-    int linesInGrid(){
-        
-    
-
-    }
-    int colsInGrid() {
-
-    }
 	void permutatePiece(int n){
-
+        Piece pref = new Piece(gen, Game.SIZE_OF_PIECE, empty, values);
+        pref.permutation(n);
 
     }
+    @Override
     void placePiece(int col) {
+        Piece pref = new Piece(gen, Game.SIZE_OF_PIECE, empty, values);
+        Piece a = pref.copy();    
         
-        Piece.copy(symbols);    
-        PlayArea.placePiece(l,col);
     }	 
+    @Override
     void generatePiece() {
+
 
     }
     boolean finished(){
