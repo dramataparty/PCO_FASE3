7dde1efea010af9698c4d893e476e2019cdf3d35
Diogo <diogobailaoforte@gmail.com>
2022-12-08 19:09:17 +0000

    A

--- ProjetoFase3Alunos/SingleScoreGame.java
+++ ProjetoFase3Alunos/SingleScoreGame.java
@@ -5,6 +5,7 @@ public class SingleScoreGame extends AbstractGame{
      * instancias representam, cuja pontuação só contabiliza a realização das jogadas, (NÃO CONTAM ELMINAÇÕES )
      * só terminam quando a àrea do jogo está copletamente vazia
      */
+    int score;
 
     public void registerPlayScore(List<Integer> eliminated){
         /* Atualiza a pontuação do jogo com os resultados de uma jogada
@@ -15,11 +16,30 @@ public class SingleScoreGame extends AbstractGame{
 
     public int score() {
         /* devolve a pontuação atual do jogo */
+        return score;
     }
     public String toString() {
+
+
         /* representação textual deste jogo, com o score */
     }
     public boolean finished(){
+        boolean fin = false;
+        for(int i =0; i < PlayArea.currentGrid.length; i++){
+            for(int e =0; e < PlayArea.currentGrid[i].length ; e++){
+                if(PlayArea.currentGrid[i][e]==Symbol.empty){
+                    fin = true;
+
+                }
+                else{
+                    fin = false;
+
+                }
+
+            }
+
+        }
+        return fin;
         /*que	devolve	true se	a	área	de	jogo	está	completamente	
 vazia;*/
     } 
