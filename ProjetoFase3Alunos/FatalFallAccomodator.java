570dfc7400c93bf34ce05702f352b5eb0a7d8c5e
Diogo <diogobailaoforte@gmail.com>
2022-12-08 19:34:50 +0000

    stuff

--- ProjetoFase3Alunos/FatalFallAccomodator.java
+++ ProjetoFase3Alunos/FatalFallAccomodator.java
@@ -1,3 +1,23 @@
-public class FatalFallAccomodator {
+public class FatalFallAccomodator implements Accomodator {
+    FatalFallAccomodator(int fatalHeight){
+        new Accomodator() {
+            /* acomoda seqs. de símbolos */
+            /* fatalHeight representa	 a	 “altura” a	 partir	 da	 qual se considera	 que	 a	
+acomodação	deve provocar a “morte”	de	todos os	símbolos “acima” dos que são	acomodados */
+        };
+        
+
+
+    }
+    accomodate(Symbol[] seq, Symbol nothing){
+        if(/* symbnum > fatalHeight */){
+
+            /* acomodatodos	os	símbolos nothing na sequência seq */
+
+
+        }
+
+    }
+
     
 }
