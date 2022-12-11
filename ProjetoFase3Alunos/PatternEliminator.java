6f938ae14311bd7712f8b6442488fdc191ef21b0
Tutuki7 <tiagotiago.f.pereira@gmail.com>
2022-12-11 18:17:47 +0000

    sussy patterns

--- ProjetoFase3Alunos/PatternEliminator.java
+++ ProjetoFase3Alunos/PatternEliminator.java
@@ -19,18 +19,18 @@ public class PatternEliminator implements Eliminator {
 
 
         int total_els = 0;
-        for(int i = 0; i<this.patterns.length ; i++){
+        for(int i = 0; i<this.patterns.size() ; i++){
             int e1count = 0;
             /* navega os padroes a eleminar, assume q têm a mesma length q um blocksize */
-            for(int e = 0; this.patterns[i].length;e++){
-                if(this.patterns[i][e] == seq[e]){
+            for(int e = 0; e < i;e++){
+                if(this.patterns[e][i] == seq[e]){
                     e1count++;
                 }
                 else{
                     e1count = 0;
                 }
             total_els += e1count;
-            if ((e1count >=3)!=0){
+            if (e1count >= 3){
                 for(int u = e; u>(u-e1count);u--){
                     seq[u]=nothing;
 
