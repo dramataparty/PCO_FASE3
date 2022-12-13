dd2e24f2ef76ae06764e384fa009c49bf45c975e
Diogo <diogobailaoforte@gmail.com>
2022-12-13 21:44:26 +0000

    aasa

--- ProjetoFase3Alunos/FatalFallAccomodator.java
+++ ProjetoFase3Alunos/FatalFallAccomodator.java
@@ -1,9 +1,12 @@
+import java.sql.SQLException;
+
 public class FatalFallAccomodator implements Accomodator {
 
     private int fatalHeight;
 
     public FatalFallAccomodator(int fatalHeight){
         this.fatalHeight = fatalHeight;
+        PlayArea.colsInGrid
             
         }
         
@@ -13,6 +16,21 @@ acomodação	deve provocar a “morte”	de	todos os símbolos “acima” dos q
 
     @Override
     public void accomodate(Symbol[] seq, Symbol nothing) {
+        for(int i=0;i<seq.length-1;i++){
+			if(seq[i]!=nothing&&seq[i+1]==nothing){
+				for(int e=seq.length-1;e>0;e--){
+					if(seq[e]!=nothing&seq[e-1]==nothing){
+						seq[e-1]=seq[i];
+						seq[i]=nothing;
+
+					}
+					
+				}
+
+			}
+		}
+	
+
 
     }
 }
