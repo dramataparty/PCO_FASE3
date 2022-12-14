cb8271a2b9a968a4ee18540911a65c5d83a4cb12
Diogo <diogobailaoforte@gmail.com>
2022-12-14 22:24:18 +0000

    onweovn

--- ProjetoFase3Alunos/PatternEliminator.java
+++ ProjetoFase3Alunos/PatternEliminator.java
@@ -16,38 +16,65 @@ public class PatternEliminator implements Eliminator {
     public int eliminateSequence(Symbol[] seq, int blockSize, Symbol nothing){
 
         /* adaptar para percorrer os patterns */
-        int i = 0, repetitions = 0;
+        int i = 0;
+        boolean matchpat;
 		Symbol symbolToVerify;
-        for(int e = 0; i<patterns.size();i++){
+        Symbol [] checkseq;
+        int eliminated = 0;
+        for(int e = 0; e<patterns.size();e++){
+
+           
+
+            checkseq = patterns.get(e);
+            int k = i;
+
+            int repetitions = 0;
             
+            while(i < seq.length && eliminated == 0){
+                symbolToVerify = seq[i];
+                matchpat = (symbolToVerify==checkseq[i]);
+                if(matchpat){
+                    repetitions++;
 
+                }
+            }
+            if(repetitions>=3){
+                eliminated+= repetitions;
 
-        }
-		int eliminated = 0;
-
-		while(i < seq.length && eliminated == 0){
-			repetitions = 0;
-			symbolToVerify = seq[i];
-
-			int k = i;
-			while(k < seq.length && symbolToVerify == seq[k]){
-				k++;
-				repetitions++;
-			}
-
-			if(symbolToVerify != nothing) {
-				eliminated = repetitions;
-
-				for(int j = i; j < k; j++)
-					seq[j] = nothing;
-			}
-
-			i = k;
-		}
-
-		return eliminated;      
+                for(int j = i; j < k; j++)
+                        seq[j] = nothing;
+             
+                   }
+            i=k;
+            }
+            return eliminated;   
             
-}
+            }
+            /* 
+            while(i < seq.length && eliminated == 0){
+                repetitions = 0;
+                symbolToVerify = seq[i];
+
+                int k = i;
+                while(k < seq.length && symbolToVerify == seq[k]){
+                    k++;
+                    repetitions++;
+                }
+
+                if(symbolToVerify == checkseq[i]) {
+                    eliminated = repetitions;
+
+                    for(int j = i; j < k; j++)
+                        seq[j] = nothing;
+                }
+
+                i = k;
+                */
+
+            
+        
+		   
+}            
+        
 
 
-}
